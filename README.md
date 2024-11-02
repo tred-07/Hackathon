f6c2885258be54a55501d89f716fb2a4413b2219
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-11-02 15:44:48 +0600

    add command

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Hackathon
 # https://docs.djangoproject.com/en/5.1/intro/tutorial01/
+# for creating app ```django-admin startapp app```
+
--- djangotutorial/mysite/settings.py
+++ djangotutorial/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d!&ilj6dix)1#toc__vh24y6i%$&s&2ts9h98zx(pnj(@qo#=+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [""]
 
 
 # Application definition
